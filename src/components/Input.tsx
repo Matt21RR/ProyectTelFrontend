29a7ac900c8c45e6c1110a48af6717ac20b7e5ba
Matt21RR <portal2matr@gmail.com>
2026-04-16 11:53:46 -0500

    Create Input.tsx

---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+type InputProps = {
+  onChange:(value:string)=>void;
+  type?: string;
+  placeholder? : string;
+}
+
+export default function Input (props: InputProps){
+  const id = window.performance.now() + Math.random() + "InputId";
+
+  return <input 
+            type={props.type ?? "text"} 
+            id={id}
+            onChange={ev => props.onChange(ev.target.value)}
+            className=" w-64 h-8 rounded-md border border-b-gray-700 m-2 px-2"
+            placeholder={props.placeholder ?? ""}
+            />
+}
