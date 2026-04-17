fbed104f42540a5f530d20e3f5b63f417b126aaa
Matt21RR <portal2matr@gmail.com>
2026-04-17 13:06:37 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import CrearCuenta from './sections/CrearCuenta.tsx'
-import Principal from './sections/Principal.tsx'
-import MenuPrincipal from './sections/MenuPrincipal.tsx'
+import IniciarSesion from './sections/IniciarSesion'
+import { readSession, writeSession } from './misc/Misc'
+import CrearCuenta from './sections/CrearCuenta'
+import Principal from './sections/Principal'
+
+
+if(readSession("section") === null){
+  //Si no hay sección en sesión, se asume que es la de login
+  writeSession("section", "login");
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Principal/>
+    {readSession("section") === "crearCuenta" ? <CrearCuenta /> : null}
+    {readSession("section") === "login" ? <IniciarSesion /> : null}
+    {readSession("section") === "principal" ? <Principal/> : null}
   </StrictMode>,
 )
