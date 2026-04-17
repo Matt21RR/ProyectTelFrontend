a4789f984a24770b39abaf1d1ec1e60de49850de
Matt21RR <portal2matr@gmail.com>
2026-04-16 21:35:42 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './sections/App.tsx'
+import CrearCuenta from './sections/CrearCuenta.tsx'
+import Principal from './sections/Principal.tsx'
+import MenuPrincipal from './sections/MenuPrincipal.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Principal/>
   </StrictMode>,
 )
