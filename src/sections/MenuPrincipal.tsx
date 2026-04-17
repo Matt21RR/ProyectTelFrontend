f0998b1a3d928503a1286400d780c9613e1e49ea
Matt21RR <portal2matr@gmail.com>
2026-04-17 13:06:28 -0500

    Update MenuPrincipal.tsx
    Cierre de sesion arreglado
    
    Si se es admin permite activar la vista de subir informacion de libros

--- src/sections/MenuPrincipal.tsx
+++ src/sections/MenuPrincipal.tsx
@@ -2,8 +2,9 @@ import { useEffect, useRef, useState } from "react";
 import {Button, MenuButton} from "../components/Button"
 import Select from "../components/Select"
 import { CARRERAS, SEMESTRES } from "../misc/FilteringInfo"
-import { apiRequest, REQUEST_METHODS } from "../misc/Misc";
+import { apiRequest, BACKEND_URLS, isAdmin, readSession, REQUEST_METHODS, writeSession } from "../misc/Misc";
 import gsap from "gsap";
+import CargarLibros from "./CargarLibros";
 
 
 type MenuPrincipalProps = {
@@ -11,8 +12,11 @@ type MenuPrincipalProps = {
 }
 
 export default function MenuPrincipal(props:MenuPrincipalProps){ //TODO add props to update Principal -> Libros recomendados list, on preferences change
-  const [carrera, setCarrera] = useState(0);
-  const [semestre, setSemestre] = useState(1);
+  const [carrera, setCarrera] = useState(readSession("carrera") ? parseInt(readSession("carrera") as string) : 1);
+  const [semestre, setSemestre] = useState(readSession("semestre") ? parseInt(readSession("semestre") as string) : 1);
+
+  const [openCargarLibrosCount, setOpenCargarLibrosCount] = useState(0);
+
   const menuRef = useRef<HTMLDivElement>(null);
   const menuBackRef = useRef<HTMLDivElement>(null);
   const lateralMenuRef = useRef<HTMLDivElement>(null);
@@ -54,40 +58,60 @@ export default function MenuPrincipal(props:MenuPrincipalProps){ //TODO add prop
   };
 
   const actualizarUsuario = ()=>{
-    apiRequest(REQUEST_METHODS.POST,"update",{carrera,semestre})
-      .then(res=>{});
-  }
-  const cerrarSesion = ()=>{
-    apiRequest(REQUEST_METHODS.POST,"logout",{})
+    apiRequest(REQUEST_METHODS.PATCH, BACKEND_URLS.AUTH, `Users/${readSession("userId")}/career-semester`, {career: carrera, semester: semestre})
       .then(res=>{
-        //TODO: Change the view to show, use SESSION storage to check if user is logged in or not, and remove session on logout
+        alert("Preferencias actualizadas");
+        writeSession("carrera", carrera.toString());
+        writeSession("semestre", semestre.toString());
         window.location.reload();
       });
+  }
+  const cerrarSesion = ()=>{
+    // apiRequest(REQUEST_METHODS.POST, BACKEND_URLS.AUTH, "logout", {})
+    //   .then(res=>{
+    //     //TODO: Change the view to show, use SESSION storage to check if user is logged in or not, and remove session on logout
+    //     window.location.reload();
+    //   });
+
+    writeSession("token", "");
+    writeSession("userId", "");
+    writeSession("isAdmin", "false");
+    writeSession("section", "login");
+    window.location.reload();
+  }
+
+  const abrirCargarLibros = ()=>{
+    setOpenCargarLibrosCount(openCargarLibrosCount + 1);
   }
 
 
   return <div className="absolute w-full h-full top-0" ref={menuRef}>
     <div className="bg-black opacity-30 w-full h-full absolute top-0 left-0" ref={menuBackRef} onClick={cerrarMenu}/>
-    <div className="relative border-r h-full w-72 shadow-2xl absolute left-0 bg-white" ref={lateralMenuRef}>
+    <div className=" border-r h-full w-72 shadow-2xl absolute left-0 bg-white" ref={lateralMenuRef}>
       <div className="w-full h-fit flex">
         <MenuButton color="bg-gray-700" action={cerrarMenu}/>
         <h3 className="my-auto">MENÚ</h3>
       </div>
 
       <div className="w-full border-y items-center flex flex-col py-2">
-        <Select onChange={setCarrera} options={CARRERAS}
+        <Select onChange={setCarrera} options={CARRERAS} selected={carrera}
           label="Carrera"
         />
-        <Select onChange={setSemestre} options={SEMESTRES}
+        <Select onChange={setSemestre} options={SEMESTRES} selected={semestre}
           label="Semestre"
         />
         <Button label="Actualizar" action={actualizarUsuario}/>
       </div>
 
-      <div className="absolute bottom-4 right-4 w-full items-end flex flex-col">
+      <div className="absolute bottom-4 right-4 w-full items-end flex flex-col gap-3">
+        {
+          isAdmin() ? <Button label="Carga Libros" action={abrirCargarLibros}/> : null
+        }
         <Button label="Cerrar Sesion" action={cerrarSesion}/>
       </div>
     </div>
+
+     { openCargarLibrosCount > 0 ? <CargarLibros openCargarLibrosCount={openCargarLibrosCount}/> : null}
 
   </div>
 
