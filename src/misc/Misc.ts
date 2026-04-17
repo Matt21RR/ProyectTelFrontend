b45753f8c9c18a6d4c8c90d7d28a28fc6686188a
Matt21RR <portal2matr@gmail.com>
2026-04-17 13:04:25 -0500

    Update Misc.ts
    agregado el metodo jquery PATCH
    
    soporte para multiples backends urls
    
    metodos para lectura y escritura en el sessionStorage agregados

--- src/misc/Misc.ts
+++ src/misc/Misc.ts
@@ -1,11 +1,17 @@
 import $ from "jquery";
-const BACKEND_URL = "http://localhost:8080";
+
+export enum BACKEND_URLS {
+  AUTH = "http://localhost:5132/api",
+  LIBRARY = "http://localhost:5281/api"
+}
+
 
 export enum REQUEST_METHODS {
   GET = "GET",
   POST = "POST",
   PUT = "PUT",
-  DELETE = "DELETE"
+  DELETE = "DELETE",
+  PATCH = "PATCH"
 }
 
 
@@ -14,14 +20,40 @@ export enum REQUEST_METHODS {
  * @param {string} url - El endpoint de la API
  * @param {object} [data] - Datos opcionales a enviar (JSON)
  */
-export function apiRequest(method: REQUEST_METHODS, endpoint: string, data: {} | null = null) {
+export function apiRequest(method: REQUEST_METHODS, baseUrl: BACKEND_URLS, endpoint: string, data: {} | null = null) {
   return new Promise((resolve, reject) => {
     $.ajax({
-        url: `${BACKEND_URL}/${endpoint}`,
+        url: `${baseUrl}/${endpoint}`, 
         type: method as string, // O 'method' en versiones modernas
         contentType: 'application/json', // Requerido comúnmente para PUT/POST
         data: data ? JSON.stringify(data) : undefined,
-        dataType: 'json'
+        dataType: 'json',
+        headers: {
+            'Authorization': 'Bearer ' + readSession('token') // Si tienes autenticación
+        },
+    })
+    .done((res) => {
+      console.log("API Request Success:", res);
+      resolve(res);
+    })
+    .fail((err) => {
+      console.error("API Request Error:", err);
+      reject(err);
+    });
+  });
+}
+
+export function apiRequestWithFormData(method: REQUEST_METHODS, baseUrl: BACKEND_URLS, endpoint: string, formData: FormData) {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: `${baseUrl}/${endpoint}`,
+      type: method as string, // O 'method' en versiones modernas
+      data: formData,
+      contentType: false, // NO enviar como JSON
+      processData: false, // NO procesar los datos
+      headers: {
+          'Authorization': 'Bearer ' + readSession('token') // Si tienes autenticación
+      }
     })
     .done((res) => {
       resolve(res);
@@ -34,9 +66,19 @@ export function apiRequest(method: REQUEST_METHODS, endpoint: string, data: {} |
 }
 
 export function writeSession(key:string, value:string){
+  console.log(`Session write: ${key} = ${value}`);
   sessionStorage.setItem(key,value);
 }
 
 export function readSession(key:string):string | null{
   return sessionStorage.getItem(key);
+}
+
+export function removeSession(key:string){
+  sessionStorage.removeItem(key);
+}
+
+
+export function isAdmin():boolean{
+  return readSession("isAdmin") === "true";
 }
