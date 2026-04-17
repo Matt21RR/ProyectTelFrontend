46ebe70760b23ad025ff213289c5b678d675f0ca
Matt21RR <portal2matr@gmail.com>
2026-04-16 21:31:48 -0500

    Create FilteringInfo.ts

---
+++ src/misc/FilteringInfo.ts
@@ -0,0 +1,18 @@
+export const CARRERAS: Array<[string,any]> = [
+  ["Ingeniería en Sistemas de Información",0],
+  ["Licenciatura en Informática",1]
+];
+
+export const SEMESTRES: Array<[string,any]> = [
+  ["Primero",1],
+  ["Segundo",2],
+  ["Tercero",3],
+  ["Cuarto",4],
+  ["Quinto",5],
+  ["Sexto",6],
+  ["Séptimo",7],
+  ["Octavo",8],
+  ["Noveno",9],
+  ["Décimo",10]
+]
+
