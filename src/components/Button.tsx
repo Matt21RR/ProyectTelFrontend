439fa86acf3d2196a34645de861750a3ef428b5a
Matt21RR <portal2matr@gmail.com>
2026-04-16 21:31:30 -0500

    Add menu and section buttons components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,41 @@
+
+
 type ButtonProps = {
   label:string;
   action: ()=>void
 }
 
-export default function Button (props: ButtonProps){
+export function Button (props: ButtonProps){
   // const id = window.performance.now() + Math.random() + "InputId";
 
-  return <div className=" cursor-pointer bg-blue-400 hover:bg-blue-500 flex w-44 rounded-md p-1" onClick={props.action}>
+  return <div className=" cursor-pointer bg-blue-400 hover:bg-blue-500  transition duration-300 flex w-40 rounded-md p-2" onClick={props.action}>
       <span className="m-auto">{props.label}</span>
+    </div>
+}
+
+type MenuButtonProps = {
+  color?:string
+  action: ()=>void
+}
+export function MenuButton (props:MenuButtonProps){
+  return <div className="w-8 h-8 flex flex-col justify-between py-1 m-2 cursor-pointer hover:rotate-90 transition duration-300" onClick={props.action}>
+    <div className={`w-full h-1 ${props.color ?? "bg-white"} rounded-sm`}/>
+    <div className={`w-full h-1 ${props.color ?? "bg-white"} rounded-sm`}/>
+    <div className={`w-full h-1 ${props.color ?? "bg-white"} rounded-sm`}/>
+  </div>
+}
+
+type SectionButtonProps = {
+  label:string;
+  isActive?:boolean;
+  action: ()=>void;
+}
+
+
+export function SectionButton (props: SectionButtonProps){
+  // const id = window.performance.now() + Math.random() + "InputId";
+
+  return <div className={`cursor-pointer text-gray-100 hover:text-white hover:bg-black/15 ${props.isActive ? "bg-black/15 text-white" : ""} flex w-40 h-[80%] border-x border-white`} onClick={props.action}>
+      <span className="m-auto font-semibold text-lg">{props.label}</span>
     </div>
 }
